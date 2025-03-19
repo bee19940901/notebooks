172700e2a8947c2ca71bf2d43bc62645580aff5a
bee19940901 <believer19940901@gmail.com>
2025-03-19 14:21:22 +0800

    m

---
+++ beetools/include/utils.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Administrator on 2025/3/19.
+//
+
+#ifndef BEETOOLS_UTILS_HPP
+#define BEETOOLS_UTILS_HPP
+
+#include "iostream"
+
+class Utils{
+public:
+    Utils() = delete;
+    static void greet(const std::string& name);
+};
+
+
+#endif //BEETOOLS_UTILS_HPP
---
+++ beetools/src/utils.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Administrator on 2025/3/19.
+//
+
+#include "../include/utils.hpp"
+
+void Utils::greet(const std::string& name) {
+    std::cout << "Hello, " << name << "!\n";
+}
