7ac04770d65cba994d05945b976336c9f7f51a2a
bee19940901 <believer19940901@gmail.com>
2025-03-25 13:28:58 +0800

    m

---
+++ CPP菜鸟教程/test4.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 2025/3/25.
+//
+
+#include "test4.hpp"
---
+++ CPP菜鸟教程/test4.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2025/3/25.
+//
+
+#ifndef CPP_TEST4_HPP
+#define CPP_TEST4_HPP
+
+#endif //CPP_TEST4_HPP
