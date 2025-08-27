ee92d164b6b43f70cc3532d7b1796e992f97eafc
bee19940901 <believer19940901@gmail.com>
2025-08-27 18:45:43 +0800

    m

---
+++ 现代CPP编程/include/chapter1_3.hpp
@@ -0,0 +1,12 @@
+//
+// Author: Bing Liang <bing@example.com>
+// Email : believer19940901@gmail.com 
+// Date  : 2025/8/27 17:36
+//
+#ifndef C___CHAPTER1_3_HPP
+#define C___CHAPTER1_3_HPP
+
+#include <iostream>
+void chapter1_3();
+
+#endif //C___CHAPTER1_3_HPP
---
+++ 现代CPP编程/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Author: Bing Liang <bing@example.com>
+// Email : believer19940901@gmail.com 
+// Date  : 2025/8/27 17:45
+//
+#include "chapter1_3.hpp"
+
+int main(){
+    chapter1_3();
+    return 0;
+}
